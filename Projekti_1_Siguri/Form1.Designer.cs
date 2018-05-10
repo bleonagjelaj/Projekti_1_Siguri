298aeea753e7aecaa8e2be3d80a8eedb805fbfff
Jetmir <Jetmir@HASANAJ>
2018-05-10 16:16:46 +0200

    Add project files.

---
+++ Projekti_1_Siguri/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Projekti_1_Siguri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlaintext = new System.Windows.Forms.Label();
+            this.lblCelesi = new System.Windows.Forms.Label();
+            this.lblCiphertext = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblMesazhi = new System.Windows.Forms.Label();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.txtCelesi = new System.Windows.Forms.TextBox();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtMesazhi = new System.Windows.Forms.TextBox();
+            this.btnFsheh = new System.Windows.Forms.Button();
+            this.btnLexo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPlaintext
+            // 
+            this.lblPlaintext.AutoSize = true;
+            this.lblPlaintext.Location = new System.Drawing.Point(23, 23);
+            this.lblPlaintext.Name = "lblPlaintext";
+            this.lblPlaintext.Size = new System.Drawing.Size(46, 13);
+            this.lblPlaintext.TabIndex = 0;
+            this.lblPlaintext.Text = "Mesazhi";
+            // 
+            // lblCelesi
+            // 
+            this.lblCelesi.AutoSize = true;
+            this.lblCelesi.Location = new System.Drawing.Point(26, 94);
+            this.lblCelesi.Name = "lblCelesi";
+            this.lblCelesi.Size = new System.Drawing.Size(55, 13);
+            this.lblCelesi.TabIndex = 1;
+            this.lblCelesi.Text = "Fjalekalimi";
+            // 
+            // lblCiphertext
+            // 
+            this.lblCiphertext.AutoSize = true;
+            this.lblCiphertext.Location = new System.Drawing.Point(26, 162);
+            this.lblCiphertext.Name = "lblCiphertext";
+            this.lblCiphertext.Size = new System.Drawing.Size(88, 13);
+            this.lblCiphertext.TabIndex = 2;
+            this.lblCiphertext.Text = "Teksi per fshehje";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(26, 295);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(55, 13);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Fjalekalimi";
+            // 
+            // lblMesazhi
+            // 
+            this.lblMesazhi.AutoSize = true;
+            this.lblMesazhi.Location = new System.Drawing.Point(26, 357);
+            this.lblMesazhi.Name = "lblMesazhi";
+            this.lblMesazhi.Size = new System.Drawing.Size(46, 13);
+            this.lblMesazhi.TabIndex = 4;
+            this.lblMesazhi.Text = "Mesazhi";
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(26, 53);
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(114, 20);
+            this.txtPlaintext.TabIndex = 5;
+            // 
+            // txtCelesi
+            // 
+            this.txtCelesi.Location = new System.Drawing.Point(26, 123);
+            this.txtCelesi.Name = "txtCelesi";
+            this.txtCelesi.Size = new System.Drawing.Size(100, 20);
+            this.txtCelesi.TabIndex = 6;
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.Location = new System.Drawing.Point(26, 202);
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(100, 20);
+            this.txtCiphertext.TabIndex = 7;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(26, 320);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 8;
+            // 
+            // txtMesazhi
+            // 
+            this.txtMesazhi.Location = new System.Drawing.Point(26, 383);
+            this.txtMesazhi.Name = "txtMesazhi";
+            this.txtMesazhi.Size = new System.Drawing.Size(100, 20);
+            this.txtMesazhi.TabIndex = 9;
+            // 
+            // btnFsheh
+            // 
+            this.btnFsheh.Location = new System.Drawing.Point(64, 252);
+            this.btnFsheh.Name = "btnFsheh";
+            this.btnFsheh.Size = new System.Drawing.Size(75, 23);
+            this.btnFsheh.TabIndex = 10;
+            this.btnFsheh.Text = "Fsheh";
+            this.btnFsheh.UseVisualStyleBackColor = true;
+            this.btnFsheh.Click += new System.EventHandler(this.btnFsheh_Click);
+            // 
+            // btnLexo
+            // 
+            this.btnLexo.Location = new System.Drawing.Point(63, 421);
+            this.btnLexo.Name = "btnLexo";
+            this.btnLexo.Size = new System.Drawing.Size(75, 23);
+            this.btnLexo.TabIndex = 11;
+            this.btnLexo.Text = "Lexo";
+            this.btnLexo.UseVisualStyleBackColor = true;
+            this.btnLexo.Click += new System.EventHandler(this.btnLexo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(220, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(211, 317);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 456);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLexo);
+            this.Controls.Add(this.btnFsheh);
+            this.Controls.Add(this.txtMesazhi);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtCiphertext);
+            this.Controls.Add(this.txtCelesi);
+            this.Controls.Add(this.txtPlaintext);
+            this.Controls.Add(this.lblMesazhi);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblCiphertext);
+            this.Controls.Add(this.lblCelesi);
+            this.Controls.Add(this.lblPlaintext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlaintext;
+        private System.Windows.Forms.Label lblCelesi;
+        private System.Windows.Forms.Label lblCiphertext;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblMesazhi;
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.TextBox txtCelesi;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtMesazhi;
+        private System.Windows.Forms.Button btnFsheh;
+        private System.Windows.Forms.Button btnLexo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Projekti_1_Siguri/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Projekti_1_Siguri
+{
+    public partial class Form1 : Form
+    {
+        private byte[] byteCiphertext;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFsheh_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+
+                
+                pictureBox1.ImageLocation = open.FileName;
+
+                Bitmap bmp = new Bitmap(pictureBox1.ImageLocation);
+                MemoryStream ms1 = new MemoryStream();
+                bmp.Save(ms1, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                byte[] byteImage = ms1.ToArray();
+                String msg1 = txtCiphertext.Text;
+                byte[] byteTeksti = Encoding.UTF8.GetBytes(msg1);
+
+                int pozita = byteImage.Length - 8;
+                for (int i = 0; i < byteTeksti.Length; i++)
+                {
+                    byteImage[pozita] = byteTeksti[i];
+                    pozita = pozita - 3;
+                }
+
+                MemoryStream ms2 = new MemoryStream(byteImage);
+                Bitmap bmp1 = new Bitmap(ms2);
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    bmp1.Save(sfd.FileName);
+
+                }
+
+
+            }
+        }
+
+        private void btnLexo_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
